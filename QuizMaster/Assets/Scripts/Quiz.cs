5d5419247b9776a536b03941ad074d5e9292bfcb
FarVillage117 <aidanlongueville117@gmail.com>
2023-10-16 12:57:18 -0400

    For Loop

--- QuizMaster/Assets/Scripts/Quiz.cs
+++ QuizMaster/Assets/Scripts/Quiz.cs
@@ -6,10 +6,18 @@ using TMPro;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
-    [SerializeField] QuestionSO questions;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+        
     }
 
 }
